b4c6b6113cc8b41d3dcb777512602a6ef79d0ad7
EronAlves1996 <eron.alves@rocketmail.com>
2024-01-29 23:17:44 -0300

    feat: make fibonacci

--- src/main/java/com/eronalves/App.java
+++ src/main/java/com/eronalves/App.java
@@ -1,5 +1,6 @@
 package com.eronalves;
 
+import java.util.Collections;
 import java.util.stream.Stream;
 
 /**
@@ -7,8 +8,27 @@ import java.util.stream.Stream;
  *
  */
 public class App {
+
+  private static class Pair<L, R> {
+    public final L left;
+    public final R right;
+
+    public Pair(L left, R right) {
+      this.left = left;
+      this.right = right;
+    }
+
+    @Override
+    public String toString() {
+      return "Pair{left=" + left + ", right=" + right + "}";
+    }
+
+  }
+
   public static void main(String[] args) {
-    Stream.iterate(0, a -> 1)
+    Stream
+        .iterate(new Pair<>(0, 1),
+            p -> new Pair<>(p.left + p.right, p.left + p.right * 2))
         .limit(20)
         .forEach(System.out::println);
   }
